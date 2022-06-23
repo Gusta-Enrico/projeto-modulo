7a4645c1bb50b0098c622c06e3d698714b3b9b51
Gustavo-Enrico <gustavoenrico238@gmail.com>
2022-06-23 12:13:20 -0300

    first commit

---
+++ funcoes.mjs
@@ -0,0 +1,25 @@
+
+
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}`;
+}
+
+
+function mostraCidade(nome, cidade) {
+    return `A cidade de ${nome} é ${cidade}`;
+}
+
+
+function mostraHobby(nome, hobby) {
+    return `O hobby de ${nome} é ${hobby}`;
+}
+
+
+function idadeFutura(futura, nome) {
+    return `A idade de ${nome} daqui a 30 anos será ${futura}`
+}
+
+
+export { mostraIdade, mostraCidade, mostraHobby, idadeFutura};
+
+
---
+++ mains.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby, idadeFutura} from "./funcoes.mjs";
+
+console.log(mostraIdade("Gustavo" , 22));
+console.log(mostraCidade("Gustavo", "São Paulo"));
+console.log(mostraHobby("Gustavo", "Futebol"));
+console.log(idadeFutura("Gustavo", 22));
